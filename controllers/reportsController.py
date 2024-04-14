534fdbb40379995aae2a6292fc7f119257dd7b00
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2024-04-14 12:12:42 -0600

    Unified report files

--- controllers/reportsController.py
+++ controllers/reportsController.py
@@ -4,7 +4,7 @@ script_location = Path(__file__).absolute()
 project_root = script_location.parent.parent
 sys.path.append(str(project_root))
 
-from services.reportsService import ServiceFoodMoreOrders, ServiceAverageOrderTime, ServiceAverageMealTime
+from services.reportsService import ServiceFoodMoreOrders, ServiceAverageOrderTime, ServiceAverageMealTime, serviceComplaintByClient, serviceComplaintByPlate, serviceWaiterEfficiency
 
 
 def ServiceFoodMoreOrdersController(fecha_ini, fecha_fin):
@@ -39,7 +39,35 @@ def ServiceAverageMealTimeController(parametros):
         return {"success": False, "error": "Error al ejecutar la consulta: " + str(e)}
 
 
-
+def getComplaintByClientController(date1, date2):
+    try:
+        result = serviceComplaintByClient(date1, date2)
+        if result["success"]:
+            return {"success": True, "data": result["data"]}
+        else:
+            return {"success": False, "error": result["error"]}
+    except Exception as e:
+        return {"success": False, "error": "Error en la solicitud: " + str(e)}
+    
+def getComplaintByPlateController(date1, date2):
+    try:
+        result = serviceComplaintByPlate(date1, date2)
+        if result["success"]:
+            return {"success": True, "data": result["data"]}
+        else:
+            return {"success": False, "error": result["error"]}
+    except Exception as e:
+        return {"success": False, "error": "Error en la solicitud: " + str(e)}
+    
+def getWaiterEfficiencyController():
+    try:
+        result = serviceWaiterEfficiency()
+        if result["success"]:
+            return {"success": True, "data": result["data"]}
+        else:
+            return {"success": False, "error": result["error"]}
+    except Exception as e:
+        return {"success": False, "error": "Error en la solicitud: " + str(e)}
 
 
 
--- services/reportsService.py
+++ services/reportsService.py
@@ -54,5 +54,47 @@ def ServiceAverageMealTime(fecha_ini, fecha_fin):
         return {"success": False, "error": "Error al obtener los datos de tiempo en comer los clientes: " + str(e)}
     finally:
         conn.close()
+
+def serviceComplaintByClient(date1, date2):
+    conn = get_connection()
+    if conn is None:
+        return {"success": False, "error": "No se pudo establecer conexión con la base de datos."}
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM queja WHERE fechahora > %s AND fechahora < %s GROUP BY clienteid, quejaid", (date1, date2))
+            result = cursor.fetchall()
+            return {"success": True, "message": "Quejas obtenidas con exito", "data": result}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+    finally:
+        conn.close()
+
+def serviceComplaintByPlate(date1, date2):
+    conn = get_connection()
+    if conn is None:
+        return {"success": False, "error": "No se pudo establecer conexión con la base de datos."}
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM queja WHERE fechahora > %s AND fechahora < %s GROUP BY platoid, quejaid", (date1, date2))
+            result = cursor.fetchall()
+            return {"success": True, "message": "Quejas obtenidas con exito", "data": result}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+    finally:
+        conn.close()
+
+def serviceWaiterEfficiency():
+    conn = get_connection()
+    if conn is None:
+        return {"success": False, "error": "No se pudo establecer conexión con la base de datos."}
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT meseroid, AVG(amabilidad_score) AS amabilidad, AVG(exactitud_score) AS exactitud FROM encuesta GROUP BY meseroid")
+            result = cursor.fetchall()
+            return {"success": True, "message": "Encuestas obtenidas obtenidas con éxito", "data": result}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+    finally:
+        conn.close()
         
 
